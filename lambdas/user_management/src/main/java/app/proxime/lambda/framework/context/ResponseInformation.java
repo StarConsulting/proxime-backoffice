3f8d00beeefe509a60cc4e12389b5aae504d8e90
camilo <camilo@febos.cl>
2018-08-06 13:43:36 -0400

    PROX-16 Crear respuestas para lambda de registro

---
+++ lambdas/user_management/src/main/java/app/proxime/lambda/framework/context/ResponseInformation.java
@@ -0,0 +1,13 @@
+package app.proxime.lambda.framework.context;
+
+import java.util.Date;
+import java.util.List;
+
+public class ResponseInformation {
+    public int code;
+    public String message;
+    public List<String> errors;
+    public Date date;
+    public long duration;
+    public String transactionId;
+}
