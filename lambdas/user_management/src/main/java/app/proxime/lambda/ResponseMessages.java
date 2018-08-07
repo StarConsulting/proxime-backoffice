194d952842dae591841a4d0ac40f0d462db85ea0
camilo <camilo@febos.cl>
2018-08-07 11:09:41 -0400

    PROX-22 Encapsular lógica de negocio de autenticación y registro

---
+++ lambdas/user_management/src/main/java/app/proxime/lambda/ResponseMessages.java
@@ -0,0 +1,35 @@
+package app.proxime.lambda;
+
+public enum ResponseMessages {
+
+    /*
+     * Register responses
+     * */
+    USERNAME_IS_ALREADY_IN_USE(1,"The username is already in use"),
+    EMAIL_IS_ALREADY_IN_USE(2,"The email is already in use"),
+
+
+    /*
+     * Authentication responses
+     * */
+    EMAIL_IS_NOT_REGISTERED(3,"The specified email is not registered"),
+    PASSWORD_IS_INCORRECT(4,"The specified credentials are incorrect"),
+    ;
+
+
+    private int code;
+    private String message;
+
+    ResponseMessages(int code, String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode(){
+        return this.code;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+}
